6e43e32eaaf29330c914ec447bd4ac99871bf8eb
Дмитрий Пацура <talk@dmtry.me>
2015-04-06 21:02:31 +0900

    Added 5

--- 2-year-2th-part/5/main.cpp
+++ 2-year-2th-part/5/main.cpp
@@ -1,28 +1,125 @@
 #include <iostream>
-#include <vector>
-#include <stack>
+#include <cmath>
 #include <algorithm>
+#include <ctime>
 
 using namespace std;
 
-#define ELEMENTS_COUNT 100
+template <class T> class BinaryTree {
+public:
+	struct Node {
+		Node *left, *right;
+		T data;
 
-int main() {
-	srand ((unsigned int) time(NULL));
+		Node () : left(NULL), right(NULL) {}
+		Node (T x) : left(NULL), right(NULL), data(x) {}
+	};
+private:
+	Node *root;
+	size_t size = 0;
 
-	auto *tmpNumbers = new stack<int>();
-	tmpNumbers->reserve(ELEMENTS_COUNT);		tmpNumbers->push_back(tmp);
+	void p_add (Node*& cur, T x);
+	bool p_find (Node* cur, T x);
+public:
+	void add (T x);
+	bool find (T x);
+	size_t getSize () { return size; }
 
+	void tree_to_array(Node* tree, T a[]);
+	void sort(T a[], int elem_total);
+};
 
-	for (int i = 0; i < ELEMENTS_COUNT; i++) {
-		int tmp = (rand() % 100) + 50;
+template <class T> void BinaryTree <T> :: p_add (Node*& cur, T x) {
+	if (!cur) {
+		cur = new Node (x);
+		return;
+	}
+	if (x < cur->data) {
+		if (!cur->left)
+			cur->left = new Node (x);
+		else
+			p_add (cur->left, x);
+	}
+	else if (x > cur->data) {
+		if (!cur->right)
+			cur->right = new Node (x);
+		else
+			p_add (cur->right, x);
+	}
+}
 
-//		cout << tmp << endl;
+template <class T> bool BinaryTree <T> :: p_find (Node* cur, T x) {
+	if (!cur) {
+		return false;
 	}
 
-	cout << "Hello, World!" << endl;
-	cout << "Max " << *max_element(tmpNumbers, tmpNumbers->end()) << endl;
-	cout << "Min " << *min_element(tmpNumbers->begin(), tmpNumbers->end()) << endl;
+	if (x < cur->data) {
+		if (!cur->left) {
+			return false;
+		}
+
+		return p_find (cur->left, x);
+	} else if (x > cur->data) {
+		if (!cur->right) {
+			return false;
+		}
+
+		return p_find (cur->right, x);
+	}
+
+	return true;
+}
+
+template <class T> void BinaryTree <T> :: add (T x) {
+	size++;
+	p_add (root, x);
+}
+
+template <class T> bool BinaryTree <T> :: find (T x) {
+	return p_find (root, x);
+}
+
+template <class T> void BinaryTree <T> :: tree_to_array(Node* tree, T a[]) {
+	static int max2 = 0;                 // счетчик элементов нового массива
+
+	if (tree == NULL) {
+		return;
+	}
+
+	tree_to_array(tree->left, a);       // обход левого поддерева
+	a[max2++] = tree->data;
+	tree_to_array(tree->right, a);      // обход правого поддерева
+}
+
+template <class T> void BinaryTree <T> :: sort(T a[], int elem_total) {
+	Node *root = NULL;
+	for (int i = 0; i < elem_total; i++) {        // проход массива и заполнение дерева
+		p_add(root, a[i]);
+		size++;
+	}
+	tree_to_array(root, a);             // заполнение массива
+}
+
+int main () {
+	BinaryTree <int> btree;
+
+	int a[6]= {
+		12,
+		4,
+		55,
+		2,
+		1,
+		5
+	};
+
+	btree.sort(a, 6);
+
+	cout << "Sorted array of int variables: " << endl;
+	for (int i = 0; i < 6; i++) {
+		cout << a[i] << " ";
+	}
+
+	cout << endl;
 
 	return 0;
 }
