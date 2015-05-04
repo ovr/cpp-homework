044f85be0ba22404eaa0d7e741f6e690692bea0d
Dmitry Patsura <talk@dmtry.me>
2015-05-04 18:16:08 +0900

    Пишу программу 2-2-struct

--- 2-year-2th-part/2/main.cpp
+++ 2-year-2th-part/2/main.cpp
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include "table_printer.h"
 
 using namespace std;
 
@@ -18,24 +19,70 @@ bool sortFunction (const Item& a, const Item& b) {
 
 
 void Print(const Item& product) {
-    cout << product.title << " " << product.name << endl;
+    cout << "|" << setw(20) << product.title
+         << "|" << setw(20) << product.name
+         << "|" << setw(10) << product.color
+         << "|" << setw(10) << product.size
+         << "|" << setw(10) << product.count
+         << "|" << endl;
+
+    cout << "----------------------------------------------------------------------------" << endl;
 }
 
 void PrintWomen(const Item& product) {
     if (!product.title.empty() && product.title.at(0) != 'M') {
-        cout << product.title << " " << product.name << endl;
+        Print(product);
     }
 }
 
 void PrintMen(const Item& product) {
     if (!product.title.empty() && product.title.at(0) == 'M') {
-        cout << product.title << " " << product.name << endl;
+        Print(product);
     }
 }
 
 std::vector<Item> products;
 
-inline void readFromFile()
+inline void addItem()
+{
+    Item item;
+
+    cout << "Add item:" << endl;
+
+    cout << "title = ";
+    cin >> item.title;
+
+    cout << "name = ";
+    cin >> item.name;
+
+
+    cout << "size = ";
+    cin >> item.size;
+
+
+    cout << "color = ";
+    cin >> item.color;
+
+
+    cout << "count = ";
+    cin >> item.count;
+
+    products.push_back(item);
+}
+
+void printTableHeader()
+{
+    cout << "============================================================================" << endl;
+    cout << "|" << setw(20) << "Title"
+    << "|" << setw(20) << "Name"
+    << "|" << setw(10) << "Color"
+    << "|" << setw(10) << "Count"
+    << "|" << setw(10) << "Size"
+    << "|" << endl;
+    cout << "============================================================================" << endl;
+}
+
+void readFromFile()
 {
     ifstream ifsStud("products.txt");
 
@@ -61,12 +108,13 @@ void menu()
 {
     cout << "Please select" << endl;
     cout << "1. Read from file" << endl;
-    cout << "2. Listing" << endl;
-    cout << "3. Sort listing" << endl;
-    cout << "4. Lising men" << endl;
-    cout << "5. Lising women" << endl;
-    cout << "6. Clear db" << endl;
-    cout << "7. Exit" << endl;
+    cout << "2. Add an item" << endl;
+    cout << "3. Listing" << endl;
+    cout << "4. Sort listing" << endl;
+    cout << "5. Lising men" << endl;
+    cout << "6. Lising women" << endl;
+    cout << "7. Clear db" << endl;
+    cout << "8. Exit" << endl;
 
     int menuItem;
     cin >> menuItem;
@@ -79,27 +127,39 @@ void menu()
             cout << "Success, read" << endl << endl;
             break;
         case 2:
+            addItem();
+            cout << "Success, adding" << endl << endl;
+            break;
+        case 3:
+            printTableHeader();
+
             for_each(products.begin(), products.end(), Print);
             cout << "Success, listing" << endl << endl;
             break;
-        case 3:
+        case 4:
             sort(products.begin(), products.end(), sortFunction);
+
+            printTableHeader();
             for_each(products.begin(), products.end(), Print);
             cout << endl << endl;
             break;
-        case 4:
+        case 5:
+
+            printTableHeader();
             for_each(products.begin(), products.end(), PrintMen);
             cout << endl << endl;
             break;
-        case 5:
+        case 6:
+
+            printTableHeader();
             for_each(products.begin(), products.end(), PrintWomen);
             cout << endl << endl;
             break;
-        case 6:
+        case 7:
             products.clear();
             cout << "Success, clear memory db" << endl << endl;
             break;
-        case 7:
+        case 8:
             cout << "Good buy" << endl;
             exit(EXIT_SUCCESS);
             break;
